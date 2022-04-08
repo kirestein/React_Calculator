a0a7a3d88b70af338c745bca760d6e50c9ea63a0
kirestein <erik.proenca2011@gmail.com>
2022-04-08 15:37:30 -0300

    Editing the props of Button

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,2 +1,13 @@
 import './Button.css'
-export const Button = props => <button className='Button' > { props.label } </button>
+export const Button = props =>
+  <button
+  onClick={ _ => props.click(props.label) }
+    className={`
+      Button
+      ${ props.operation ? 'operation' : '' }
+      ${ props.double ? 'double' : '' }
+      ${ props.triple ? 'triple' : '' }
+    `} 
+    >
+    {props.label}
+  </button>
